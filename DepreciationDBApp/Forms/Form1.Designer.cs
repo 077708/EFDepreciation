c7f73f6e167d08daf5087c5c4563e59556ba4cd5
Paulo Jirón <mayorga07708@gmail.com>
2022-05-17 22:38:34 -0600

    Adding data

---
+++ DepreciationDBApp/Forms/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿
+namespace DepreciationDBApp.Forms
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnAddAsset = new System.Windows.Forms.Button();
+            this.dgvAsset = new System.Windows.Forms.DataGridView();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvAsset)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnAddAsset
+            // 
+            this.btnAddAsset.Location = new System.Drawing.Point(46, 26);
+            this.btnAddAsset.Name = "btnAddAsset";
+            this.btnAddAsset.Size = new System.Drawing.Size(75, 23);
+            this.btnAddAsset.TabIndex = 0;
+            this.btnAddAsset.Text = "Add Asset";
+            this.btnAddAsset.UseVisualStyleBackColor = true;
+            this.btnAddAsset.Click += new System.EventHandler(this.btnAddAsset_Click);
+            // 
+            // dgvAsset
+            // 
+            this.dgvAsset.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvAsset.Location = new System.Drawing.Point(23, 65);
+            this.dgvAsset.Name = "dgvAsset";
+            this.dgvAsset.RowTemplate.Height = 25;
+            this.dgvAsset.Size = new System.Drawing.Size(506, 230);
+            this.dgvAsset.TabIndex = 1;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(596, 65);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 23);
+            this.textBox1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(741, 358);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.dgvAsset);
+            this.Controls.Add(this.btnAddAsset);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvAsset)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnAddAsset;
+        private System.Windows.Forms.DataGridView dgvAsset;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
